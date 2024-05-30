5e831fb5546d8db2d7feb8cacfc89aba13d554ae
kesaari <alisa189189@yamdex.ru>
2024-05-30 14:27:58 +0300

    доработки

--- script.js
+++ script.js
@@ -1,63 +1,32 @@
-var menuBtn = document.querySelector('.navigation__menu');
-var submenu = document.querySelector('menu');
-var closeBtn = document.querySelector('.close')
+let menuBtn = document.querySelector('.navigation__menu');
+let submenu = document.querySelector('menu');
+let closeBtn = document.querySelector('.close')
+let overlay = document.querySelector('.overlay')
+let menuShow = false;
 
 menuBtn.addEventListener('click', function() {
+  if (menuShow == false) { 
     submenu.style.display = "block";
+    menuShow = true;
+} else {
+  submenu.style.display = "none";
+  menuShow = false;
+}
 });
 
 closeBtn.addEventListener('click', function() {
     submenu.style.display = "none";
+    menuShow = false;
 });
+overlay.addEventListener('click', function() {
+  submenu.style.display = "none";
+  menuShow = false;
+});
+
 
 document.addEventListener('keydown', function (evt) {
     if (evt.keyCode === 27) {
         submenu.style.display = "none";
+        menuShow = false;
     }});
 
-
-var items = document.querySelectorAll('.display--none');
-var seeMoreBtn = document.querySelector('.see-more');
-
-
-seeMoreBtn.addEventListener('click', function() {
-  items.classList.toggle('display--none')
-});
-
-// Получаем элементы слайдера
-const slider = document.querySelector('.slider');
-const prevButton = document.querySelector('.prev-button');
-const nextButton = document.querySelector('.next-button');
-const slides = Array.from(slider.querySelectorAll('.profit__slide'));
-const slideCount = slides.length;
-let slideIndex = 0;
-
-// Устанавливаем обработчики событий для кнопок
-prevButton.addEventListener('click', showPreviousSlide);
-nextButton.addEventListener('click', showNextSlide);
-
-// Функция для показа предыдущего слайда
-function showPreviousSlide() {
-  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
-  updateSlider();
-}
-
-// Функция для показа следующего слайда
-function showNextSlide() {
-  slideIndex = (slideIndex + 1) % slideCount;
-  updateSlider();
-}
-
-// Функция для обновления отображения слайдера
-function updateSlider() {
-  slides.forEach((slide, index) => {
-    if (index === slideIndex) {
-      slide.style.display = 'block';
-    } else {
-      slide.style.display = 'none';
-    }
-  });
-}
-
-// Инициализация слайдера
-updateSlider();
