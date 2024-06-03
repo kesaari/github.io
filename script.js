427e6e60054b6b2d15b1e79608f81291fcab0925
kesaari <alisa189189@yamdex.ru>
2024-06-03 09:52:00 +0300

    dfhsetjn

--- script.js
+++ script.js
@@ -30,26 +30,25 @@ document.addEventListener('keydown', function (evt) {
         menuShow = false;
     }});
 
-    const swiper = new Swiper('.swiper', {
-      // Optional parameters
-      loop: true,
-    
-      // If we need pagination
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      }
-    });
+    let swiper = null;
 
-    let windowWidth = documentElement.clientWidth
-    let x = window.matchMedia("(max-width: 768px)")
- 
-      if (windowWidth <= 768) {
-        Swiper ()
-      }
+    function sliderStart() {
+       if (!swiper) {
+        swiper = new Swiper('.swiper', {
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            slidesPerView: 'auto',
+          }
+        });
+       }
+    }
 
     window.addEventListener ('resize', function() {
       if (window.matchMedia("(max-width: 768px)").matches) {
-        console.log('aboba')
+        sliderStart()
+      } else {
+        swiper.disable();
+        swiper = null;
       }
     })
